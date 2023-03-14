9dfb887bf7ebd0d706562847b06a3ea89a5a5164
Dheeraj Bisht <dheerajbisht876@gmail.com>
2023-03-14 20:10:53 +0530

    client and server socket connection

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,29 @@
-console.log("Gello");
+import express from "express";
+import { createServer } from "http";
+import { Server } from "socket.io";
+import cors from "cors";
+
+const PORT = process.env.PORT || 8080;
+
+const app = express();
+app.use(cors);
+const httpServer = createServer(app);
+
+const io = new Server(httpServer, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("connection estd.", socket.id);
+
+  socket.on("disconnect", (reason, desc) =>
+    console.log("disconnected", reason, desc)
+  );
+});
+
+httpServer.listen(PORT, () => {
+  console.log("Listening on port", PORT);
+});
