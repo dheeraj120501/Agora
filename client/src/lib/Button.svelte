8e36510f49ffc3abf11633f6f04b8a7dd8e369f7
Dheeraj Bisht <dheerajbisht876@gmail.com>
2023-03-15 00:23:50 +0530

    Buttons component added

---
+++ client/src/lib/Button.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">    
+    export let value;
+    export let onClick;
+</script>
+
+<button class="bg-blue-400 px-4 py-2 text-white rounded-lg hover:bg-blue-500 transition-all text-lg" on:click={onClick}>{value}</button>
